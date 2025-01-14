f3533b305cafc39dcaa28ec00eb03b33a7aa9bfb
palasmert <mertpalas39@gmail.com>
2025-01-14 18:34:27 +0300

    improved navigation bar

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -13,8 +13,24 @@ struct ContentView: View {
     
     //MARK: - BODY
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top,
+                             UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 0)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }//:VSTACK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }//: ZSTACK
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
