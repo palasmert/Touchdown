c31b987c2faaee12cf502c37c8ba41787b3d58c9
palasmert <mertpalas39@gmail.com>
2025-01-14 14:27:06 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Mert Palas on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
