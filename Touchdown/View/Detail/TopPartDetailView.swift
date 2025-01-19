c5c701dd9e126f0d5b15c70f58788089a9e2ded7
palasmert <mertpalas39@gmail.com>
2025-01-20 01:13:42 +0300

    add product detail screen components

---
+++ Touchdown/View/Detail/TopPartDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  TopPartDetailView.swift
+//  Touchdown
+//
+//  Created by Mert Palas on 20.01.2025.
+//
+
+import SwiftUI
+
+struct TopPartDetailView: View {
+    //MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack(alignment: .center, spacing: 6, content: {
+            // PRICE
+            VStack(alignment: .leading, spacing: 6, content: {
+                Text("Price")
+                    .fontWeight(.semibold)
+                
+                Text(sampleProduct.formattedPrice)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .scaleEffect(1.40, anchor: .leading)
+            })//: VSTACK
+            .offset(y: isAnimating ? -50 : 75)
+            
+            Spacer()
+            
+            // PHOTO
+            Image(sampleProduct.image)
+                .resizable()
+                .scaledToFit()
+                .offset(y: isAnimating ? 0 : -325)
+            
+        })//: HSTACK
+        .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.75)) {
+                isAnimating.toggle()
+            }
+        })
+    }
+}
+
+struct TopPartDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopPartDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
